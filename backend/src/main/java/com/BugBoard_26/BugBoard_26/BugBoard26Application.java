b7d4eaa5bc90067f76c9a3df2e280044796919fd
SaViFaNk03 <sasy.03@hotmail.it>
2025-12-05 15:09:34 +0100

    feat: initialize Angular frontend application with routing and basic structure
    initialize Spring Boot backend application with a basic structure

---
+++ backend/src/main/java/com/BugBoard_26/BugBoard_26/BugBoard26Application.java
@@ -0,0 +1,13 @@
+package com.BugBoard_26.BugBoard_26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugBoard26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugBoard26Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/BugBoard_26/BugBoard_26/BugBoard26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BugBoard_26.BugBoard_26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugBoard26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
