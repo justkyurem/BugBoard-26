0f4679b314c5288eedeb1eceeb13f04e12e76197
SaViFaNk03 <sasy.03@hotmail.it>
2026-01-27 11:34:41 +0100

    Aggiornato il file angular.json per abilitare l'analisi, aggiunto il provider HttpClient in app.config.ts, rimosso il contenuto di placeholder in app.html e definito il routing per issue-board in app.routes.ts.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { IssueBoardComponent } from './components/issue-board/issue-board.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', redirectTo: 'issue-board', pathMatch: 'full'
+    },
+    {
+        path: 'issue-board', component: IssueBoardComponent
+    }
+
+    //
+];
